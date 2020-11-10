68bdd3a74a771e81db2049cdb13f1232650adc34
dujingsheng <dujingsheng@4zlink.com>
2020-11-10 17:17:42 +0800

    ribbon

---
+++ consumer8001/src/main/java/com/pig1et/ribbon/rule.java
@@ -0,0 +1,20 @@
+package com.pig1et.ribbon;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @description: 指定ribbon的轮询策略，注意此配置类一定不能放到@ComponentScan可以扫描到的包下
+ * @author: pig1etO
+ * @create: 2020-11-10 17:11
+ **/
+@Configuration
+public class rule {
+
+    @Bean
+    public IRule getRibbonRule() {
+        return new RandomRule();
+    }
+}
