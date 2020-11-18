00ed158a9730ea1ebd04d2ffb92d2cb855edf462
dujingsheng <dujingsheng@4zlink.com>
2020-11-18 11:25:30 +0800

    GateWay开始

---
+++ gateway-80/src/main/java/com/pig1et/study/gateway80/Gateway80Application.java
@@ -0,0 +1,13 @@
+package com.pig1et.study.gateway80;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gateway80Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gateway80Application.class, args);
+    }
+
+}
---
+++ gateway-80/src/test/java/com/pig1et/study/gateway80/Gateway80ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pig1et.study.gateway80;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gateway80ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
